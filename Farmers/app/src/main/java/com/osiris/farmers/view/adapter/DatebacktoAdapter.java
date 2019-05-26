edacadce14a51c786a68fc21d5df4804500e2aeb
Mr_Ashin <Mr_Ashin@Mr-Ashin-2.local>
2019-05-27 07:03:59 +0800

    提交追溯页面

---
+++ Farmers/app/src/main/java/com/osiris/farmers/view/adapter/DatebacktoAdapter.java
@@ -0,0 +1,4 @@
+package com.osiris.farmers.view.adapter;
+
+public class DatebacktoAdapter {
+}
--- Farmers/app/src/main/java/com/osiris/farmers/view/fragment/DatebacktoFragment.java
+++ Farmers/app/src/main/java/com/osiris/farmers/view/fragment/DatebacktoFragment.java
@@ -1,11 +1,20 @@
 package com.osiris.farmers.view.fragment;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.osiris.farmers.R;
 import com.osiris.farmers.base.BaseFragment;
 
+import butterknife.BindView;
+
 public class DatebacktoFragment extends BaseFragment {
+
+
+
+	@BindView(R.id.rv_data)
+	RecyclerView rv_data;
+
 	@Override
 	protected int setLayout() {
 		return R.layout.fragment_datebackto;
